aa60cc7c73e67564a048ccfbed3baf5a58ec04f0
Samuel  Weston <samuelwestonphoto@gmail.com>
2024-09-05 16:00:30 +0100

    spinner-css/

--- react-bootstrap/src/app/components/select.js
+++ react-bootstrap/src/app/components/select.js
@@ -1,13 +1,18 @@
-import Form from 'react-bootstrap/Form';
+import Form from "react-bootstrap/Form";
 
 function SelectBasicExample() {
   return (
-    <Form.Select aria-label="Default select example">
-      <option>Open this select menu</option>
-      <option value="1">One</option>
-      <option value="2">Two</option>
-      <option value="3">Three</option>
-    </Form.Select>
+    <div className="info">
+      <h1> Hello </h1>
+      <p> </p>
+
+      <Form.Select className="selectmenu">
+        <option> DropDown </option>
+        <option>Clothing </option>
+        <option> Menswear</option>
+        <option>T-shirts</option>
+      </Form.Select>
+    </div>
   );
 }
 
--- react-bootstrap/src/app/select/page.js
+++ react-bootstrap/src/app/select/page.js
@@ -2,12 +2,15 @@ import Form from "react-bootstrap/Form";
 
 function SelectBasicExample() {
   return (
-    <Form.Select className="selectmenu" aria-label="Default select example">
-      <option>Open this select menu</option>
-      <option value="1">One</option>
-      <option value="2">Two</option>
-      <option value="3">Three</option>
-    </Form.Select>
+    <div className="info">
+      <h1> Hello </h1>
+      <Form.Select className="selectmenu">
+        <option> Dropdown </option>
+        <option>Clothing </option>
+        <option> Menswear</option>
+        <option>T-shirts</option>
+      </Form.Select>
+    </div>
   );
 }
 
