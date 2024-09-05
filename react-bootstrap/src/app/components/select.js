09a3368ba6c948658c6d891ea8a7c721fe6fc97e
Samuel  Weston <samuelwestonphoto@gmail.com>
2024-09-05 16:17:38 +0100

    paragraph/select

--- react-bootstrap/src/app/components/select.js
+++ react-bootstrap/src/app/components/select.js
@@ -3,14 +3,24 @@ import Form from "react-bootstrap/Form";
 function SelectBasicExample() {
   return (
     <div className="info">
-      <h1> Hello </h1>
-      <p> </p>
+      <h1> Dropdown Menu </h1>
+      <p>
+        {" "}
+        A select dropdown menu is a user interface element commonly used in
+        forms to allow users to choose one option from a predefined choices. It
+        displays a compant, clicable box that when clicked; reveals a vertical
+        list of options. Users can scroll through the options and select their
+        preferred choice.
+      </p>
 
       <Form.Select className="selectmenu">
         <option> DropDown </option>
-        <option>Clothing </option>
-        <option> Menswear</option>
-        <option>T-shirts</option>
+        <option> Latest </option>
+        <option> Shoes + </option>
+        <option>Clothing +</option>
+        <option> Brands + </option>
+        <option> Blog </option>
+        <option> Sale </option>
       </Form.Select>
     </div>
   );
--- react-bootstrap/src/app/select/page.js
+++ react-bootstrap/src/app/select/page.js
@@ -3,12 +3,24 @@ import Form from "react-bootstrap/Form";
 function SelectBasicExample() {
   return (
     <div className="info">
-      <h1> Hello </h1>
+      <h1> Dropdown Menu </h1>
+      <p className="text">
+        {" "}
+        A select dropdown menu is a user interface element commonly used in
+        forms to allow users to choose one option from a predefined choices. It
+        displays a compant, clicable box that when clicked; reveals a vertical
+        list of options. Users can scroll through the options and select their
+        preferred choice.
+      </p>
+
       <Form.Select className="selectmenu">
-        <option> Dropdown </option>
-        <option>Clothing </option>
-        <option> Menswear</option>
-        <option>T-shirts</option>
+        <option> DropDown </option>
+        <option> Latest </option>
+        <option> Shoes + </option>
+        <option>Clothing +</option>
+        <option> Brands + </option>
+        <option> Blog </option>
+        <option> Sale </option>
       </Form.Select>
     </div>
   );
